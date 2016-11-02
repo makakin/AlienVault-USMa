c4a3aefdabc986a8127d98bb5a8845bb22cf6729
scottymace <scottymace@gmail.com>
2016-11-02 11:33:11 -0500

    Made backward compatible to powershell 2.0

--- nxlog-inst/inst-nxlog.ps1
+++ nxlog-inst/inst-nxlog.ps1
@@ -1,4 +1,5 @@
 ﻿#Installer script for Alienvault USM Anywhere NXLog integration
+#Tested with Powershell v2 - v5. 
 #Must allow script execution in powershell, and must be run as administrator
 #Note that webclient commented out stuff is testing potential autodownloading of configs.
 # Set Some variables
@@ -11,29 +12,46 @@ $nxpath = "C:\Program Files (x86)\nxlog\conf\"
 $nxcfg = "nxlog.conf"
 $sypath = "C:\Sysmon"
 $sycfg = "sysmon_config.xml"
+$scriptPath = split-path -parent $MyInvocation.MyCommand.Definition
+#Test if files and script are in the same directory
+If (-Not (Test-Path "$scriptPath\$nxcfg") -or (-Not(Test-Path "$scriptPath\$sycfg")) -or (-Not(Test-Path "$scriptPath\Sysmon.zip"))) {
+Write-Host "You must run this script from within a folder containing the supplied $nxcfg $sycfg and Sysmon.zip"
+exit 1 
+} Else {
+#Function to expand file in PSv2
+function Expand-ZIPFile($file, $destination)
+{
+$shell = new-object -com shell.application
+$zip = $shell.NameSpace($file)
+foreach($item in $zip.items())
+{
+$shell.Namespace($destination).copyhere($item, 0x14)
+}
+}
 #$WebClient = New-Object System.Net.WebClient
-Write-Host "Download the latest version of NxLog to this folder.  Your default browser will now connect you"
+Write-Host "Download the latest version of NxLog to this folder: $scriptPath.  Your default browser will now connect you"
 Start 'https://nxlog.co/products/nxlog-community-edition/download'
 Write-Host "Press any key to continue ..."
 $x = $host.UI.RawUI.ReadKey("NoEcho,IncludeKeyDown")
-Start-Process -FilePath "./nxlog*.msi" -ArgumentList /quiet -Wait 
-#$WebClient.DownloadFile("http://10.60.50.60/nxlog.conf","C:\Program Files (x86)\nxlog\conf\nxlog.conf")
+Start-Process (Resolve-Path "$scriptPath\nxlog*.msi") -ArgumentList /quiet -Wait
+#$WebClient.DownloadFile("http://<someurl>/nxlog.conf","$scriptPath")
 If (Test-Path $nxpath\$nxcfg){
   Copy-Item "$nxpath\$nxcfg" "$nxpath\$nxcfg-$(get-date -f yyyyMMddhhmmss)"
-  Copy-Item $nxcfg "$nxpath\$nxcfg"
+  Copy-Item $scriptPath\$nxcfg "$nxpath\$nxcfg"
 }Else{
- Copy-Item $nxcfg "$nxpath\$nxcfg"
+ Copy-Item $scriptPath\$nxcfg "$nxpath\$nxcfg"
 }
-(Get-Content "$nxpath\$nxcfg").replace('[USMIPADDRESS]', $sensorip) | Set-Content "$nxpath\$nxcfg"
+(Get-Content "$nxpath\$nxcfg") -replace '\[USMIPADDRESS\]', $sensorip | Set-Content $nxpath\$nxcfg
 #Will install sysmon
-Expand-Archive Sysmon.zip $sypath -Force
+ New-Item -ItemType Directory -Path C:\Sysmon -Force
+ Expand-ZIPFile "$scriptpath\Sysmon.zip" -destination $sypath -Force
+ #$WebClient.DownloadFile("http://<someurl>/sysmon_config.xml","$scriptPath")
 If (Test-Path $sypath\$sycfg){
   Copy-Item "$sypath\$sycfg" "$sypath\$sycfg-$(get-date -f yyyyMMddhhmmss)"
-  Copy-Item $sycfg "$sypath\$sycfg"
+  Copy-Item $scriptPath\$sycfg "$sypath\$sycfg"
 }Else{
- Copy-Item $sycfg "$sypath\$sycfg"
+ Copy-Item $scriptPath\$sycfg "$sypath\$sycfg"
 }
-#$WebClient.DownloadFile("http://10.60.50.60/sysmon_config.xml","C:\Sysmon\sysmon_config.xml")
 Start-Process -FilePath "$sypath\Sysmon.exe" -ArgumentList "-accepteula -h md5 -n -l -i $sypath\$sycfg"
 #Let's start nxlog too
 Start-Service -InputObject nxlog
@@ -41,8 +59,9 @@ Start-Service -InputObject nxlog
 Enable-PSRemoting –force
 #Let's make sure we save the current TrustedHosts and add the USM to the list
 $curValue = (get-item wsman:\localhost\Client\TrustedHosts).value 
-IF([string]::IsNullOrWhiteSpace($curValue)) {            
+IF([string]::IsNullOrEmpty($curValue) -or $curValue -contains "*") {         
     Set-Item wsman:\localhost\Client\TrustedHosts -value "$SensorIP"            
 } else {            
     Set-Item wsman:\localhost\Client\TrustedHosts -value "$curValue, $SensorIP"            
-}     
+} 
+}    
